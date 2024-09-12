1dde2404181b04ec711ba09be2fbadb5e2093013
Asish Mehata <mehataasish@gmail.com>
2024-09-12 19:20:21 +0545

    BASICS

---
+++ addition.py
@@ -0,0 +1,14 @@
+<<<<<<< HEAD
+#addition
+=======
+#ADDITION
+>>>>>>> origin/main
+
+num1= float(input("Enter the first number for addition: "))
+num2= float(input("Enter the second number for addition: "))
+sum_result = num1 + num2
+<<<<<<< HEAD
+print(f"sum: {num1} + {num2} = {sum_result}" )
+=======
+print(f"sum: {num1} + {num2} = {sum_result}" )
+>>>>>>> origin/main
