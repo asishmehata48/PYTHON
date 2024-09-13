d4ada969ccdbca6ffd918793cf840540d37bf690
Asish Mehata <mehataasish@gmail.com>
2024-09-13 19:38:52 +0545

    DAY 02

---
+++ DAY 02/lists.py
@@ -0,0 +1,15 @@
+# Creating a list
+fruits = ["apple", "banana", "cherry"]
+
+# Accessing elements
+print(fruits[0])  # Output: apple
+
+# Adding an element
+fruits.append("orange")
+
+# Removing an element
+fruits.remove("banana")
+
+# Looping through the list
+for fruit in fruits:
+    print(fruit)
