edaeb2b121ddf307595089f81f750c58204e8254
Asish Mehata <mehataasish@gmail.com>
2024-09-13 20:06:43 +0545

    LISTS

---
+++ DAY 02/dictionaries.py
@@ -0,0 +1,8 @@
+student = {"name": "John", "age": 21, "grade": "A"}
+
+# Accessing dictionary values
+print(student["name"])  # Output: John
+
+# Adding a new key-value pair
+student["major"] = "Computer Science"
+print(student)
