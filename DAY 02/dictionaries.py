4ef61b500a5b4b43230b6e1b3e93bc7d24dd9907
Asish Mehata <166911195+asishmehata48@users.noreply.github.com>
2024-10-15 06:31:09 +0545

    Update dictionaries.py

--- DAY 02/dictionaries.py
+++ DAY 02/dictionaries.py
@@ -2,6 +2,7 @@ student = {"name": "John", "age": 21, "grade": "A"}
 
 # Accessing dictionary values
 print(student["name"])  # Output: John
+
 # Adding a new key-value pair
 student["major"] = "Computer Science"
 print(student)
