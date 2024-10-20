8dbf0f13eb7c814946eb8e2a618cfd823f8e164d
Asish Mehata <mehataasish@gmail.com>
2024-10-20 11:03:32 +0545

    assigning values to multiple variables

--- Variables/multiplevariables.py
+++ Variables/multiplevariables.py
@@ -1,5 +1,5 @@
-a, b, c = 5, 3.2, 'Hello'
+a, b, c = 9, 3.4, 'AYO'
 
-print (a)  # prints 5
-print (b)  # prints 3.2
-print (c)  # prints Hello 
+print (a)  # prints 9
+print (b)  # prints 3.4
+print (c)  # prints AYO
