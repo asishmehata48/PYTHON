bf7aecf0699dffcca14da875855727bc8b72a500
Asish Mehata <mehataasish@gmail.com>
2024-10-20 19:05:03 +0545

    LITERALS ADDED

---
+++ Variables/usingliterals.py
@@ -0,0 +1,27 @@
+# boolean Literals
+is_pass = True
+print(is_pass)
+
+# character literals
+some_character = 'S'
+print(some_character)
+
+# special literals
+value = None
+print(value)
+
+# list literal
+fruits = ["apple", "mango", "orange"] 
+print(fruits)
+
+# tuple literal
+numbers = (1, 2, 3) 
+print(numbers)
+
+# dictionary literal
+alphabets = {'a':'apple', 'b':'ball', 'c':'cat'} 
+print(alphabets)
+
+# set literal
+vowels = {'a', 'e', 'i' , 'o', 'u'} 
+print(vowels)
