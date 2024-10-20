19b4747b999458126e12a52cfc7219a0a0fddd02
Asish Mehata <mehataasish@gmail.com>
2024-10-20 19:05:45 +0545

    Literals used

--- Variables/usingliterals.py
+++ Variables/usingliterals.py
@@ -2,26 +2,26 @@
 is_pass = True
 print(is_pass)
 
-# character literals
-some_character = 'S'
-print(some_character)
+# # character literals
+# some_character = 'S'
+# print(some_character)
 
-# special literals
-value = None
-print(value)
+# # special literals
+# value = None
+# print(value)
 
-# list literal
-fruits = ["apple", "mango", "orange"] 
-print(fruits)
+# # list literal
+# fruits = ["apple", "mango", "orange"] 
+# print(fruits)
 
-# tuple literal
-numbers = (1, 2, 3) 
-print(numbers)
+# # tuple literal
+# numbers = (1, 2, 3) 
+# print(numbers)
 
-# dictionary literal
-alphabets = {'a':'apple', 'b':'ball', 'c':'cat'} 
-print(alphabets)
+# # dictionary literal
+# alphabets = {'a':'apple', 'b':'ball', 'c':'cat'} 
+# print(alphabets)
 
-# set literal
-vowels = {'a', 'e', 'i' , 'o', 'u'} 
-print(vowels)
+# # set literal
+# vowels = {'a', 'e', 'i' , 'o', 'u'} 
+# print(vowels)
