ca1de8bce812b1f52624c8493ddc13328711b438
Asish Mehata <166911195+asishmehata48@users.noreply.github.com>
2024-11-01 16:52:07 +0545

    Update output.py

--- Fundamentals/output.py
+++ Fundamentals/output.py
@@ -3,3 +3,19 @@ x = 10
 y = 5
 
 print('The value of x is {} and the value of y is {}' .format(x,y))
+
+
+name = "John"
+score = 95.12345
+
+# Using format() for precision and alignment
+print("Hello, {}! Your score is {:.2f}".format(name, score))
+
+
+# Using triple quotes for multiline strings
+message = """Hello, World!
+This is a multiline text.
+Enjoy coding in Python!"""
+print(message)
+
+
