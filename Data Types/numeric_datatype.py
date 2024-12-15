fcae72672f10976bdd5a9251eccb6147fc871a87
Asish Mehata <mehataasish@gmail.com>
2024-12-15 23:11:50 +0545

    numeric data type added

---
+++ Data Types/numeric_datatype.py
@@ -0,0 +1,11 @@
+# Integer
+x = 10
+print(type(x))  # Output: <class 'int'>
+
+# Float
+y = 10.5
+print(type(y))  # Output: <class 'float'>
+
+# Complex
+z = 2 + 3j
+print(type(z))  # Output: <class 'complex'>
