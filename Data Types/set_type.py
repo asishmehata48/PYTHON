0f3131898d69987cdceee435e799b90330a1804e
Asish Mehata <mehataasish@gmail.com>
2024-12-15 23:13:44 +0545

    set data type added

---
+++ Data Types/set_type.py
@@ -0,0 +1,7 @@
+# Set
+set_example = {1, 2, 3, 4}
+print(type(set_example))  # Output: <class 'set'>
+
+# Frozenset
+frozenset_example = frozenset([1, 2, 3])
+print(type(frozenset_example))  # Output: <class 'frozenset'>
