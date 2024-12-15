c2f01a8ff247f4ee770b1d72a5b547f213bcc995
Asish Mehata <mehataasish@gmail.com>
2024-12-15 23:16:22 +0545

    binary data type added

---
+++ Data Types/binary_datatype.py
@@ -0,0 +1,11 @@
+# Bytes
+b = b"Hello"
+print(type(b))  # Output: <class 'bytes'>
+
+# Bytearray
+ba = bytearray(b"Hello")
+print(type(ba))  # Output: <class 'bytearray'>
+
+# Memoryview
+mv = memoryview(b"Hello")
+print(type(mv))  # Output: <class 'memoryview'>
