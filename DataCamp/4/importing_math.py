2a2eea896c03a4394f6599b7f037794770c1c394
Asish Mehata <mehataasish@gmail.com>
2025-08-18 23:40:10 +0545

    import packages

--- DataCamp/4/importing_math.py
+++ DataCamp/4/importing_math.py
@@ -9,3 +9,17 @@ A = math.pi * 0.43 ** 2
 
 print("Circumference: " + str(C))
 print("Area: " + str(A))
+
+
+
+# Import pi function of math package
+from math import pi
+
+# Calculate C
+C = 2 * 0.43 * pi
+
+# Calculate A
+A = pi * 0.43 ** 2
+
+print("Circumference: " + str(C))
+print("Area: " + str(A))
