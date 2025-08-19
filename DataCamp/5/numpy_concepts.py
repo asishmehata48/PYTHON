2a450f540e7a78471d1fcfa4b5e23110df842874
Asish Mehata <mehataasish@gmail.com>
2025-08-19 23:42:40 +0545

    used NumPy concepts for slicing, printing lists

---
+++ DataCamp/5/numpy_concepts.py
@@ -0,0 +1,41 @@
+# Import the numpy package as np
+import numpy as np
+
+baseball = [180, 215, 210, 210, 188, 176, 209, 200]
+
+# Create a numpy array from baseball: np_baseball
+np_baseball = np.array(baseball)
+
+# Print out type of np_baseball
+print(type(np_baseball))
+
+
+
+# Import numpy
+import numpy as np
+
+# Create a numpy array from height_in: np_height_in
+np_height_in = np.array(height_in)
+
+# Print out np_height_in
+print(np_height_in)
+
+# Convert np_height_in to m: np_height_m
+np_height_m = np_height_in * 0.0254
+
+# Print np_height_m
+print(np_height_m)
+
+
+
+
+import numpy as np
+
+np_weight_lb = np.array(weight_lb)
+np_height_in = np.array(height_in)
+
+# Print out the weight at index 50
+print(np_weight_lb[50])
+
+# Print out sub-array of np_height_in: index 100 up to and including index 110
+print(np_height_in[100:111])
